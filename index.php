a31252ac2516576048988825d88cb79288c83283
Wan-Lin <kuku.bag@gmail.com>
2018-10-31 10:17:29 +0000

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,9 @@
                 <p style="font-size: 12px; color: #f5f5f5;">
                   <a href="https://www.facebook.com/askPiPA" target="_blank" style="color: #333333; text-decoration: none;">Facebook</a> |
                   <a href="https://twitter.com/PiPAsays" target="_blank" style="color: #333333; text-decoration: none;">Twitter</a> |
-                  <a style="color: #f0626e; text-decoration: none; font-weight: bold;" href="https://www.pipa.ai" target="_blank">pipa.ai</a></p>
+                  <a style="color: #f0626e; text-decoration: none; font-weight: bold;" href="https://www.pipa.ai" target="_blank">pipa.ai</a>
+                  <p style="font-size: 10px">This message and any attachments are confidential and should only be read by the intended recipient. If you are not the intended recipient, please inform us by replying to this message, delete the message from your computer and destroy any copies. Any distribution or copying without prior permission is prohibited. This e-mail and any attachments are legally privileged.</p>                
+              </p>
             </td>
           </tr>
        </table>
